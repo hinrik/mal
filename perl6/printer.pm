ba833c56cc935571c8bd990e7fe5a006af5e370e
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2016-06-12 14:14:20 +0000

    Reduce some code repetition in the printer
    This way $print_readably doesn't have to be mentioned in multiple places.
    A more explicit way to do this would be:
    
      &reprint = &pr_str.assuming(*, $print_readably);
    
    But assuming() in Rakudo is really slow at the moment.
    
    See https://rt.perl.org/Ticket/Display.html?id=128388

--- perl6/printer.pm
+++ perl6/printer.pm
@@ -2,17 +2,18 @@ unit module printer;
 use types;
 
 sub pr_str ($exp, $print_readably = False) is export {
+  my &reprint = { pr_str($^a, $print_readably) };
   given $exp {
     when MalFunction { "#<fn* ({$exp.params}) {pr_str($exp.ast)}>" }
     when MalCode { "#<builtin_fn* {$exp.fn.gist}>" }
     when MalList {
-      '(' ~ join(' ', |$exp.map({ pr_str($_, $print_readably) })) ~ ')';
+      '(' ~ join(' ', |$exp.map({ &reprint($_) })) ~ ')';
     }
     when MalVector {
-      '[' ~ join(' ', |$exp.map({ pr_str($_, $print_readably) })) ~ ']';
+      '[' ~ join(' ', |$exp.map({ &reprint($_) })) ~ ']';
     }
     when MalHashMap {
-      '{' ~ $exp.kv.flatmap({ MalString($^a), $^b }).map({ pr_str($_, $print_readably) }) ~ '}'
+      '{' ~ $exp.kv.flatmap({ MalString($^a), $^b }).map({ &reprint($_) }) ~ '}'
     }
     when MalString {
       my $str = $exp.val;
@@ -23,7 +24,7 @@ sub pr_str ($exp, $print_readably = False) is export {
         '"' ~ $str.trans(/\\/ => '\\\\', /\"/ => '\\"', /\n/ => '\\n') ~ '"';
       }
     }
-    when MalAtom { "(atom {pr_str($exp.val, $print_readably)})" }
+    when MalAtom { "(atom {&reprint($exp.val)})" }
     when MalValue { $exp.val }
   }
 }
